c08f642cbf2aa417612f8ef7302da0132a3713c1
Danesh <138777868+Danesh1520@users.noreply.github.com>
2024-06-24 10:53:59 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,42 +1,6 @@
 import streamlit as st
 import base64
 
-# Add custom CSS to change the background color to black
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: white;
-        color: black;
-    }
-    .sidebar .sidebar-content {
-        background: black;
-    }
-    .stTextInput>div>div>input {
-        color: white;
-        background-color: #333;
-    }
-    .stTextArea>div>div>textarea {
-        color: white;
-        background-color: #333;
-    }
-    .stTextInput>div>div>label, .stTextArea>div>div>label {
-        color: white;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.title("Medical Code Chatbot")
 question = st.text_input("Question", key="input1", placeholder="Type your question here", help="Enter your medical question here.")
 answer = st.text_input("Answer", key="input2", placeholder="Answer will appear here", help="The answer to your question will be displayed here.")
-
-if st.button("Get Answer"):
-    if question:
-        # Here you would call your model to get the answer (placeholder for now)
-        # Replace the following line with your LLM code to get the answer
-        answer = "This is a placeholder answer."  
-        st.text_area("Answer", value=answer, placeholder="Answer will appear here", help="The answer to your question will be displayed here.", height=200)
-    else:
-        st.warning("Please enter a question before submitting.")
