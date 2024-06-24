ceabc99cb5ac6e2d7f33c3ca719d8872fc9c71fd
Danesh <138777868+Danesh1520@users.noreply.github.com>
2024-06-24 11:13:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from langchain_community.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
-# Initialize the LlamaCpp model
 llm = LlamaCpp(
     model_path="downloads/llama-2-7b-chat.Q4_0.gguf",
     n_gpu_layers=40,
@@ -11,7 +10,6 @@ llm = LlamaCpp(
     verbose=False
 )
 
-# Define the prompt template
 template = """
 Question: {question}
 
@@ -19,16 +17,11 @@ Answer:
 """
 prompt = PromptTemplate(template=template, input_variables=["question"])
 
-# Create an LLM chain with the prompt and the LLM
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-# Streamlit application
 st.title("Medical Code Chatbot")
-
-# Input for the user's question
 question = st.text_input("Question", key="input1", placeholder="Type your question here", help="Enter your medical question here.")
 
-# Display the answer when the question is submitted
 if question:
     answer = llm_chain.run(question)
     st.text_area("Answer", value=answer, key="input2", placeholder="Answer will appear here", help="The answer to your question will be displayed here.", height=200)
